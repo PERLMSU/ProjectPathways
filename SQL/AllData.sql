6c62d4ea1ff3f8e2a861c655d4ef02f35033f5b8
John Aiken <johnm.aiken@gmail.com>
2015-10-02 16:01:45 +0900

    Update AllData.sql

--- SQL/AllData.sql
+++ SQL/AllData.sql
@@ -1,16 +1,56 @@
+USE [MSU_ProjectPathways]
+GO
 
-SELECT *
-FROM [MSU_ProjectPathways].[dbo].[Students] AS s
-JOIN [MSU_ProjectPathways].[dbo].[PhysicsCourses] AS p
-ON s.PlaceholderID = p.PlaceholderID
-JOIN MSU_ProjectPathways.dbo.MajorCodes AS mj
-ON mj.major_code = s.major_code
+/****** Object:  Table [dbo].[AllData]    Script Date: 02-Oct-15 15:58:27 ******/
+DROP TABLE [dbo].[AllData]
+GO
 
-UNION
+/****** Object:  Table [dbo].[AllData]    Script Date: 02-Oct-15 15:58:27 ******/
+SET ANSI_NULLS ON
+GO
 
-SELECT *
-FROM [MSU_ProjectPathways].[dbo].[Students] AS s
-JOIN MSU_ProjectPathways.dbo.MathCourses AS m
-ON s.PlaceholderID = m.PlaceholderID
-JOIN MSU_ProjectPathways.dbo.MajorCodes AS mj
-ON mj.major_code = s.major_code
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[AllData](
+	ID int IDENTITY(1,1) PRIMARY KEY,
+	[PlaceholderID] [varchar](50) NULL,
+	[gndr_flag] [varchar](50) NULL,
+	[citizenship] [varchar](50) NULL,
+	[Ethnicity] [varchar](50) NULL,
+	[First Gen] [varchar](50) NULL,
+	[undergrad Residency] [varchar](50) NULL,
+	[primary major] [varchar](50) NULL,
+	[degree conferred for this major] [varchar](50) NULL,
+	[graduated MSU] [varchar](50) NULL,
+	[graduated MSU primary major] [varchar](50) NULL,
+	[startTermSeqID] [varchar](50) NULL,
+	[EndTermSeqID] [varchar](50) NULL,
+	[first term in major] [varchar](50) NULL,
+	[most recent term in major] [varchar](50) NULL,
+	[pre-major class level] [varchar](50) NULL,
+	[most recent term in major class level] [varchar](50) NULL,
+	[PhysicsTermSeqID] [varchar](50) NULL,
+	[PhysicsTermCode] [varchar](50) NULL,
+	[SubjectCode] [varchar](50) NULL,
+	[crse_code] [varchar](50) NULL,
+	[sctn_code] [varchar](50) NULL,
+	[grade_code] [varchar](50) NULL,
+	[Major] [varchar](50) NULL,
+	[coll_code] [varchar](50) NULL,
+	[college] [varchar](50) NULL,
+	[department] [varchar](50) NULL,
+	[award_type] [varchar](50) NULL,
+	[degree type] [varchar](50) NULL,
+	[first primary bachelor degree] [varchar](50) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
