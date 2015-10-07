5b61ad2c401911029880affe3626141befc78e07
John Aiken <johnm.aiken@gmail.com>
2015-10-08 00:19:10 +0900

    Update PopulateAllData.sql

--- SQL/PopulateAllData.sql
+++ SQL/PopulateAllData.sql
@@ -1,14 +1,16 @@
-INSERT INTO MSU_ProjectPathways.dbo.AllData
-SELECT *
-FROM
-(
-SELECT  s.[PlaceholderID]
+
+-- ALL DATA
+select *
+into AllData
+from (
+select s.[id]
+      ,s.[PlaceholderID]
       ,s.[gndr_flag]
       ,s.[citizenship]
       ,s.[Ethnicity]
       ,s.[First Gen]
       ,s.[undergrad Residency]
-      --,s.[major_code]
+      ,s.[major_code]
       ,s.[primary major]
       ,s.[degree conferred for this major]
       ,s.[graduated MSU]
@@ -19,35 +21,27 @@ SELECT  s.[PlaceholderID]
       ,s.[most recent term in major]
       ,s.[pre-major class level]
       ,s.[most recent term in major class level]
-      ,p.[term_seq_id] AS PhysicsTermSeqID
-      ,p.[term_code] AS PhysicsTermCode
-      ,p.[subj_code] AS SubjectCode
+	  --,p.[PlaceholderID]
+      ,p.[term_seq_id]
+      ,p.[term_code]
+      ,p.[subj_code]
       ,p.[crse_code]
       ,p.[sctn_code]
       ,p.[grade_code]
-	  ,mj.[long_desc] AS Major
-      ,mj.[coll_code]
-      ,mj.[college]
-      --,mj.[dept_code]
-      ,mj.[department]
-      ,mj.[award_type]
-      ,mj.[degree type]
-      ,mj.[first primary bachelor degree]
-FROM [MSU_ProjectPathways].[dbo].[Students] AS s
-JOIN [MSU_ProjectPathways].[dbo].[PhysicsCourses] AS p
-ON s.PlaceholderID = p.PlaceholderID
-JOIN MSU_ProjectPathways.dbo.MajorCodes AS mj
-ON mj.major_code = s.major_code
+from Students_cleaned as s
+left join PhysicsCourses as p
+on s.PlaceholderID = p.PlaceholderID
 
-UNION
+union
 
-SELECT s.[PlaceholderID]
+select s.[id]
+      ,s.[PlaceholderID]
       ,s.[gndr_flag]
       ,s.[citizenship]
       ,s.[Ethnicity]
       ,s.[First Gen]
       ,s.[undergrad Residency]
-      --,s.[major_code]
+      ,s.[major_code]
       ,s.[primary major]
       ,s.[degree conferred for this major]
       ,s.[graduated MSU]
@@ -58,25 +52,14 @@ SELECT s.[PlaceholderID]
       ,s.[most recent term in major]
       ,s.[pre-major class level]
       ,s.[most recent term in major class level]
+	  --,m.[PlaceholderID]
       ,m.[term_seq_id]
       ,m.[term_code]
       ,m.[subj_code]
       ,m.[crse_code]
       ,m.[sctn_code]
       ,m.[grade_code]
-	  --,mj.[major_code]
-      ,mj.[long_desc] AS Major
-      ,mj.[coll_code]
-      ,mj.[college]
-      --,mj.[dept_code]
-      ,mj.[department]
-      ,mj.[award_type]
-      ,mj.[degree type]
-      ,mj.[first primary bachelor degree]
-
-FROM [MSU_ProjectPathways].[dbo].[Students] AS s
-JOIN MSU_ProjectPathways.dbo.MathCourses AS m
-ON s.PlaceholderID = m.PlaceholderID
-JOIN MSU_ProjectPathways.dbo.MajorCodes AS mj
-ON mj.major_code = s.major_code
-) AS t
+from Students_cleaned as s
+left join MathCourses as m
+on s.PlaceholderID = m.PlaceholderID
+) as t
