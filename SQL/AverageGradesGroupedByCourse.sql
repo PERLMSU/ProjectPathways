70f0526105fdfb5cb017ddb0f5c5c88d6317ce63
John Aiken <johnm.aiken@gmail.com>
2015-10-13 21:03:56 +0900

    Update AverageGradesGroupedByCourse.sql

--- SQL/AverageGradesGroupedByCourse.sql
+++ SQL/AverageGradesGroupedByCourse.sql
@@ -1,34 +1,34 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 DECLARE @cleaned TABLE (id INT IDENTITY(1,1) NOT NULL PRIMARY KEY
-						, PlaceholderID VARCHAR(50) NOT NULL
-						, term_seq_id VARCHAR(50)
-						, term_code VARCHAR(50)
-						, subj_crse_code VARCHAR(50)
-						, grade_code FLOAT--VARCHAR(50)
-						, UNIQUE(id))
+			, PlaceholderID VARCHAR(50) NOT NULL
+			, term_seq_id VARCHAR(50)
+			, term_code VARCHAR(50)
+			, subj_crse_code VARCHAR(50)
+			, grade_code FLOAT--VARCHAR(50)
+			, UNIQUE(id))
 
 INSERT INTO @cleaned
 SELECT PlaceholderID
-, term_seq_id
-, term_code
---, subj_code + '_' + crse_code
-, CASE 
-	WHEN crse_code LIKE '%A%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
-	WHEN crse_code LIKE '%B%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
-	WHEN crse_code LIKE '%C%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
-	WHEN crse_code LIKE '%E%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
-	ELSE subj_code + '_' + crse_code
-END
-, CASE 
-	WHEN grade_code = 'DF' THEN 5.0
-	WHEN grade_code = 'I' THEN 6.0
-	WHEN grade_code = 'N' THEN 7.0
-	WHEN grade_code = 'P' THEN 8.0
-	WHEN grade_code = 'U' THEN 9.0
-	WHEN grade_code = 'V' THEN 10.0
-	WHEN grade_code = 'W' THEN 11.0
-	ELSE grade_code
-END
+	, term_seq_id
+	, term_code
+	--, subj_code + '_' + crse_code
+	, CASE 
+		WHEN crse_code LIKE '%A%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
+		WHEN crse_code LIKE '%B%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
+		WHEN crse_code LIKE '%C%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
+		WHEN crse_code LIKE '%E%' THEN subj_code + '_' + LEFT(crse_code, LEN(crse_code) - 1)
+		ELSE subj_code + '_' + crse_code
+	END
+	, CASE 
+		WHEN grade_code = 'DF' THEN 5.0
+		WHEN grade_code = 'I' THEN 6.0
+		WHEN grade_code = 'N' THEN 7.0
+		WHEN grade_code = 'P' THEN 8.0
+		WHEN grade_code = 'U' THEN 9.0
+		WHEN grade_code = 'V' THEN 10.0
+		WHEN grade_code = 'W' THEN 11.0
+		ELSE grade_code
+	END
   FROM [MSU_ProjectPathways].[dbo].[AllCourses]
   WHERE subj_code != '' -- REMOVE BLANKS
   AND crse_code NOT LIKE '%Q%' -- REMOVE COURSES FROM QUARTER SYSTEM
@@ -38,9 +38,9 @@ END
 
  /****** EVERYONE ******/
 --SELECT subj_crse_code
---, COUNT(DISTINCT PlaceholderID)
---, AVG(grade_code)
---, STDEV(grade_code)
+	--, COUNT(DISTINCT PlaceholderID)
+	--, AVG(grade_code)
+	--, STDEV(grade_code)
 --FROM @cleaned
 --WHERE grade_code <= 4.0
 --GROUP BY subj_crse_code
@@ -48,9 +48,9 @@ END
 
 /****** GRADUATES ******/
 SELECT c.subj_crse_code
-, COUNT(DISTINCT c.PlaceholderID)
-, AVG(c.grade_code)
-, STDEV(c.grade_code)
+	, COUNT(DISTINCT c.PlaceholderID)
+	, AVG(c.grade_code)
+	, STDEV(c.grade_code)
 FROM Students_cleaned AS s
 LEFT JOIN @cleaned AS c
 ON s.PlaceholderID = c.PlaceholderID
